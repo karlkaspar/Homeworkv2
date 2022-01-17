e85c6e23d83bd5b9714c347ba6346e2992907f2f
karlkaspar <karlkasparkulesha@gmail.com>
2022-01-17 15:03:20 +0200

    Basic form step change logic

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,3 +1,14 @@
 $(window).ready(() => {
     console.log("I am ready")
+
+
+    setStep = (el, from) => {
+        let currentStep = $('.step.active').attr('stepId')
+        let stepWanted = $(el).parent().attr('stepId')
+        console.log('stepWanted', stepWanted, 'curStep', currentStep)
+
+        $('.step').removeClass('active')
+        $('.step[stepId=' + stepWanted + ']').addClass('active')
+
+    }
 })
