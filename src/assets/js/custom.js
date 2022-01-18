799f6e1b876d6cb7f3bb2af84ae5267b4e86e906
karlkaspar <karlkasparkulesha@gmail.com>
2022-01-18 12:36:15 +0200

    Final touches

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -39,13 +39,16 @@ $(window).ready(() => {
     }
 
     validator = (curstep, newStep) => {
+
         if (curstep == 0 && newStep == 1) { // First step of the form
             $('#intro').hide()
             return true
         }
+
         if (curstep == 1 && curstep > newStep) {
             location.reload()
         }
+
         if (curstep != 5 && newStep < 5) {
             toggleInputsDisabled(false)
         } else if (curstep == 5 && newStep < 5) {
@@ -53,9 +56,11 @@ $(window).ready(() => {
             toggleInputsDisabled(false)
             return true
         }
+        
         if (newStep < curstep) { // Going back to any of the previous steps
             return true // If we have any errors on our current step, we can always go back, but not forward
         }
+
         if (curstep == 1) { // Step 1 validation
             if (!$("section[stepid='1'] input[type='text']").val() || $("section[stepid='1'] input:radio:checked").length == 0) {
                 showError(curstep)
@@ -64,9 +69,11 @@ $(window).ready(() => {
                 return true
             }
         }
+
         if (curstep == 2) { // Step 2 validation, second step does not need validation as there are only optional checkboxes there
             return true
         }
+        
         if (curstep == 3) { // Step 3 validation
             if ($("section[stepid='3'] select").val()) {
                 return true
@@ -75,18 +82,18 @@ $(window).ready(() => {
                 return false
             }
         }
+
         if (curstep == 4) { // Step 4 validation
-            if ($("section[stepid='3'] textarea").val()) {
+            if ($("section[stepid='4'] textarea").val() && newStep == 5) { // User continued to step 5, show all form parts
+                toggleInputsDisabled(true)
+                toggleAllFormSections(true)
                 return true
             } else {
                 showError(curstep)
+                return false
             }
         }
-        if (curstep == 4 & newStep == 5) { // User continued to step 5, show all form parts
-            toggleInputsDisabled(true)
-            toggleAllFormSections(true)
-            return true
-        }
+      
         if (curstep == 5 && newStep == 6) {
             if ($("section[stepid='5'] .conditions").is(":checked")) { // Hide all form sections and present finished section 
                 setFormFinished()
